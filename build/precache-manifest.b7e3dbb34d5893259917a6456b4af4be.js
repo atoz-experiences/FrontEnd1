585fa81d99c7ed499c3bba53c059859fb4fc6901
Benjamin <benjamindoyle97@gmail.com>
2019-09-01 22:35:08 -0700

    npm build

---
+++ build/precache-manifest.b7e3dbb34d5893259917a6456b4af4be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4066002d837048aa3bd077bc965097",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaa643f3f0230d088090",
+    "url": "/static/css/main.7bda2425.chunk.css"
+  },
+  {
+    "revision": "8615f4ebd0f7ff12197d",
+    "url": "/static/js/2.5ea3ec51.chunk.js"
+  },
+  {
+    "revision": "eaa643f3f0230d088090",
+    "url": "/static/js/main.4b3510be.chunk.js"
+  },
+  {
+    "revision": "f123c8df1e602ede3122",
+    "url": "/static/js/runtime~main.7ead3a18.js"
+  }
+]);
